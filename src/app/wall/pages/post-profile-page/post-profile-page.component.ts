cb3f05d5806768ee2ba40eca05bde900a3f82caf
Mariusz Rolik <mariusz.rolik@softiq.pl>
2022-06-09 15:17:29 +0200

    post profile child

--- src/app/wall/pages/post-profile-page/post-profile-page.component.ts
+++ src/app/wall/pages/post-profile-page/post-profile-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostsService } from 'src/app/posts/services/posts.service';
 import { Post } from '../../interfaces/post.interface';
 
@@ -14,12 +14,22 @@ export class PostProfilePageComponent implements OnInit {
 
   constructor(
     private activatedRoot: ActivatedRoute,
-    private postsService: PostsService
+    private postsService: PostsService,
+    private router: Router
   ) { }
 
   async setupPost() {
     const postId = this.activatedRoot.snapshot.params["id"];
-    this.post = await this.postsService.getPostsById(postId);
+  
+    try {
+      const post = await this.postsService.getPostsById(postId);
+      if (!post) {
+        throw new Error('Post is not found')
+      }
+      this.post = post;
+    } catch {
+      this.router.navigate(['post-not-found'])
+    }
   }
 
   ngOnInit(): void {
