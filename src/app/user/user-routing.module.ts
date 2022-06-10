aa4afbadccc5abedce5e65a15da6a56dea0f7dea
Mariusz Rolik <mariusz.rolik@softiq.pl>
2022-06-10 11:26:42 +0200

    guards

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OnlyForAuthUserGuard } from './guards/only-for-auth-user.guard';
 import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 import { SignOutPageComponent } from './pages/sign-out-page/sign-out-page.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
@@ -8,7 +9,8 @@ import { UserProfilePageComponent } from './pages/user-profile-page/user-profile
 const routes: Routes = [
   {
     path: 'profile',
-    component: UserProfilePageComponent
+    component: UserProfilePageComponent,
+    canActivate: [OnlyForAuthUserGuard]
   },
   {
     path: 'register',
